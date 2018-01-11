c5a90c323cd84ada8cabc09c8b3c651eac6297bb
Jeremy Moon <jeremy.moon16@gmail.com>
2018-01-11 16:25:05 -0600

    Change design

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -22,3 +22,9 @@ $(document).ready(function(){
     } // End if
   });
 });
+
+$(document).on('click','.navbar-collapse.in',function(e) {
+    if( $(e.target).is('a') ) {
+        $(this).collapse('hide');
+    }
+});
